1a11a89599059910b39d9e1a53bdff7577b6807e
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-05-11 15:45:49 -0400

    BE: connect to heroku server

--- spotify-clone-client/src/services/Api.js
+++ spotify-clone-client/src/services/Api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 import { config } from 'dotenv'
 
-export const BASE_URL = 'http://localhost:4000'
+export const BASE_URL = 'https://ltbl-spotify-clone-server-2879f0b3ab97.herokuapp.com/'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 

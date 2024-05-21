7465f236cf56e0dabc88ee23ef1b0e3fb616fdde
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-05-21 18:38:37 -0400

    STYLE: Drawer

--- spotify-clone-client/src/pages/PlaylistDetailScreen.jsx
+++ spotify-clone-client/src/pages/PlaylistDetailScreen.jsx
@@ -13,6 +13,7 @@ const PlaylistDetailScreen = () => {
   const [loaded, setLoaded] = useState(false);
   const [info, setInfo] = useState(null);
   const [tracks, setTracks] = useState(null);
+  const [scrolling, setScrolling] = useState(false)
   const scrollRef = useRef(null);
 
   const fetchInfo = async () => {
@@ -34,24 +35,48 @@ const PlaylistDetailScreen = () => {
     }
   }, [tracks, info]);
 
-const handleScroll = (e) => {
-  const scrollPosition = window.scrollY;
-  console.log(scrollPosition)
-}
+  const handleScroll = (e) => {
+    if (scrollRef.current) {
+
+      const scrollPosition = window.scrollY;
+      const element = scrollRef.current.offsetTop;
+      console.log(element, scrollPosition)
+      if (scrollPosition > element - 100) {
+        setScrolling(true)
+      }
+      if (scrollPosition === 0) {
+        setScrolling(false)
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, []);
 
   return loaded ? (
-    <div className="relative w-full h-screen no-scrollbar flex items-bottom justify-center ">
-      <div className="div h-[1000px]"></div>
+    <div
+      className="relative w-full h-screen no-scrollbar flex items-bottom justify-center "
+    >
       <div className="w-full fixed top-0 z-0">
         <img src={info.images[0].url} alt="" className="w-full" />
       </div>
-      <div className="w-full h-[95%] z-10 bg-stone-900 bottom-[-45%] absolute overflow-scroll pb-36 rounded-t-3xl no-scrollbar ">
-        <div className="container pt-8">
-          <div className="main-title">{info.name}</div>
-          <div className="text-sm font-thin text-stone-400">
-            {info.description}
+      <div
+        ref={scrollRef}
+        className={scrolling ? 'after-scroll' : 'before-scroll'}
+      >
+        <div className="relative">
+          <div className={scrolling ? 'top-0 pb-4 bg-stone-900 sticky pt-6 container bottom-shadow' : 'top-0 bg-stone-900 bottom-shadow pt-6 pb-4 container'}>
+            <div className=" main-title">{info.name}</div>
+            <div className=" text-sm font-thin text-stone-400">
+              {info.description}
+            </div>
           </div>
-          <div className="pt-8 flex flex-col gap-2">
+          <div className="container pt-8 flex flex-col gap-2">
             {tracks.map((song) => (
               <IndividualSongAndMenu
                 key={song.track.id}
